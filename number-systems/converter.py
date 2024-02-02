f55fbddf1b779c657345416759a6dd8f88451c4c
sininen-blue <alaanneil@gmail.com>
2024-02-02 19:09:58 +0800

    added bin to hex

--- number-systems/converter.py
+++ number-systems/converter.py
@@ -1,4 +1,3 @@
-# TODO make floating poitn work
 def binToDecimal(input):
     isNegative = False
 
@@ -21,6 +20,50 @@ def binToDecimal(input):
         return -output
 
 
+def binToHex(input):
+    binHex = {
+        "0000": "0",
+        "0001": "1",
+        "0010": "2",
+        "0011": "3",
+        "0100": "4",
+        "0101": "5",
+        "0110": "6",
+        "0111": "7",
+        "1000": "8",
+        "1001": "9",
+        "1010": "A",
+        "1011": "B",
+        "1100": "C",
+        "1101": "D",
+        "1110": "E",
+        "1111": "F",
+    }
+
+    numList = [int(x) for x in str(input)]
+    numList.reverse()
+
+    while len(numList) % 4 != 0:
+        numList.append(0)
+    numList.reverse()
+
+    output = []
+
+    count = 0
+    nibble = []
+    for number in numList:
+        nibble.append(number)
+        count += 1
+        if count == 4:
+            toConvert = "".join(map(str, nibble))
+            output.append(binHex[toConvert])
+
+            count = 0
+            nibble = []
+
+    return "".join(map(str, reversed(output)))
+
+
 def decToBinary(input):
     output = []
     isNegative = False
@@ -38,7 +81,7 @@ def decToBinary(input):
     if isNegative is False:
         return int(s)
     else:
-        return int("-"+s)
+        return int("-" + s)
 
 
-print(binToDecimal(-100100))
+print(binToHex(101100))
